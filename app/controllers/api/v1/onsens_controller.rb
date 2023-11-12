a3a9312d66f394fbf0035d6e0fade1641c34b7ee
Takamizu-Kenta <kenta.takamizu@potepan.jp>
2023-11-12 21:13:02 +0900

    feat/ONSEN_RAILS_002_温泉周りを実装

---
+++ app/controllers/api/v1/onsens_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::OnsensController < ApplicationController
+  def index
+    @onsens = Onsen.all
+    render json: @onsens
+  end
+
+  def create
+    @onsen = Onsen.new(onsen_params)
+
+    if @onsen.save
+      render json: @onsen, status: :created, location: @onsen
+    else
+      render json: @onsen.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @onsen.update(onsen_params)
+      render json: @onsen
+    else
+      render json: @onsen.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @onsen.destroy
+    head :no_content
+  end
+end
