eb1ab6efd574f9147cdea2391c821bc0235afc20
Takamizu-Kenta <kenta.takamizu@potepan.jp>
2023-12-30 22:18:04 +0900

    fix/ONSEN-30_ルーティングを使用しないため

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :onsens, only: [:index, :show, :create, :update, :destroy] do
+      resources :onsens, only: [:index, :show, :create, :update, :destroy], defaults: { format: :json } do
         collection do
           get 'all'
         end
       end
-      resources :facilities, only: [:index, :show, :create, :update, :destroy] do
-        collection do
-          get 'all'
-        end
-      end
-      resources :prefectures, only: [:index, :show, :create, :update, :destroy]
+      resources :facilities, only: [:index, :show, :create, :update, :destroy], defaults: { format: :json }
+      resources :prefectures, only: [:index, :show, :create, :update, :destroy], defaults: { format: :json }
     end
   end
 end
