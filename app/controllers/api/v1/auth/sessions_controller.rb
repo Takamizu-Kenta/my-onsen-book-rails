62f8cbf1153be880448a02a1eceb57e9c39ce1ec
Takamizu-Kenta <kenta.takamizu@potepan.jp>
2024-01-10 00:12:02 +0900

    fix/ONSEN-24_ログイン情報が保持できていなかったため

---
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
+  skip_before_action :verify_authenticity_token, raise: false
+
+  # ログイン処理
+  def create
+    # user = User.find_by(email: params[:email])
+    # if user && user.valid_password?(params[:password])
+
+    #   # トークンを生成
+    #   token = user.create_new_auth_token
+
+    #   # トークン情報をJSON形式で返す
+    #   render json: {
+    #     data: user.as_json.merge({
+    #       access_token: token['access-token'],
+    #       client: token['client'],
+    #       uid: token['uid']
+    #     })
+    #   }, status: :ok
+    # else
+    #   render json: { error: 'Invalid email or password' }, status: :unauthorized
+    # end
+
+    super
+  end
+end
