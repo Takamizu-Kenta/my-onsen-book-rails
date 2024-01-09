dc4e381f901c73c6646a239d122fcb85bc250b41
Takamizu-Kenta <kenta.takamizu@potepan.jp>
2024-01-10 00:11:57 +0900

    feat/ONSEN-24_devise, letter_openerを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   namespace :api do
     namespace :v1 do
       post 'facility_registrations', to: 'facility_registrations#create'
@@ -11,6 +13,14 @@ Rails.application.routes.draw do
       resources :facilities, only: [:index, :show, :create, :update, :destroy], defaults: { format: :json }
       resources :prefectures, only: [:index, :show, :create, :update, :destroy], defaults: { format: :json }
       resources :facility_types, only: [:index, :show, :create, :update, :destroy], defaults: { format: :json }
+
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        sessions: 'api/v1/auth/sessions'
+      }
+
+      namespace :current do
+        resource :user, only: [:show]
+      end
     end
   end
 end
