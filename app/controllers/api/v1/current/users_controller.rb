6b9ea48ae58d91daf4184aa51d9439015bbb39e2
Takamizu-Kenta <kenta.takamizu@potepan.jp>
2024-01-10 12:08:25 +0900

    refactor/ONSEN-24_rubocop指摘修正

--- app/controllers/api/v1/current/users_controller.rb
+++ app/controllers/api/v1/current/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Current::UsersController < Api::V1::BaseController
-	before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def show
     render json: current_user, serializer: CurrentUserSerializer
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-	skip_before_action :verify_authenticity_token
-	include DeviseTokenAuth::Concerns::SetUserByToken
-	include DeviseHackFakeSession
-	include ActionController::Cookies
+  skip_before_action :verify_authenticity_token
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseHackFakeSession
+  include ActionController::Cookies
 end
