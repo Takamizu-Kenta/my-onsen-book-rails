4a8c6d1dc6548bc4d9a669c953c2200c1d01d975
Takamizu-Kenta <kenta.takamizu@potepan.jp>
2024-01-10 12:10:53 +0900

    refactor/ONSEN-24_rubocop指摘修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# セッションが保持できないためActiveRecord::Baseを使用
 class User < ActiveRecord::Base # rubocop:disable Rails/ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
